6306a376eb0c0b8be913c672775734971f4fc214
Mostafa-Ibrahim1 <78238174+Mostafa-Ibrahim1@users.noreply.github.com>
2021-11-08 21:15:27 +0200

    Constructors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 class MyPhoneBook
@@ -19,9 +19,39 @@ class MyPhoneBook
  int* findByPhone(string);
  bool updateNameAt(string, int);
  bool updatePhoneAt(string, int);
- ~MyPhoneBook();
+//  ~MyPhoneBook();
 };
+//Constructors
+MyPhoneBook::MyPhoneBook(int phoneBookSize){
+    this->phoneBookSize=phoneBookSize;
+    names=new string[phoneBookSize];
+    phones=new string[phoneBookSize];
+    for(int i=0;i<phoneBookSize;i++){
+        names[i]=" ";
+        phones[i]=" ";
+        }
+}
+
+MyPhoneBook::MyPhoneBook(const MyPhoneBook &phoneBook){
+    phoneBookSize=phoneBook.phoneBookSize;
+    names=new string[phoneBookSize];
+    phones=new string[phoneBookSize];
+    for(int i=0;i<phoneBookSize;i++){
+        names[i]=phoneBook.names[i];
+        phones[i]=phoneBook.phones[i];
+    }
+}
+
+
+
+
 
 int main(){
+    //Used for testing functionality
+
+    // MyPhoneBook pb1(4);
+    // bool result=pb1.addEntry("Mostafa","123456780010");
+    // cout<<result<<endl;
+    // MyPhoneBook pb2(pb1);
     return 0;
 }
