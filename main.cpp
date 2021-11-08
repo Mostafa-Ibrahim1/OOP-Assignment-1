7640abc9054a102ad9330638c34516b61e005a1f
Mostafa-Ibrahim1 <78238174+Mostafa-Ibrahim1@users.noreply.github.com>
2021-11-08 21:17:40 +0200

    addEntry Function

--- main.cpp
+++ main.cpp
@@ -42,6 +42,45 @@ MyPhoneBook::MyPhoneBook(const MyPhoneBook &phoneBook){
     }
 }
 
+//Add Entry
+
+bool MyPhoneBook::addEntry(string name ,string phone){
+    bool entryAddedSuccessfully=false;
+    string allowedPhoneNumberCharacters="0123456789";
+    bool isPhoneNumberValid=false;
+
+    if(phone.length()==11){
+        for(int i=0;i<phone.length();i++){
+            if(allowedPhoneNumberCharacters.find(phone[i])!=string::npos){
+                isPhoneNumberValid=true;
+            }else{
+                isPhoneNumberValid=false;
+                break;
+            }
+    }
+    }else{
+        isPhoneNumberValid=false;
+    }
+
+    // This is one of the conditions that leads to entry insertion failure (Phone number being not valid)
+    if(isPhoneNumberValid==false){
+        return false;
+    }
+    //Then it will check if there is an available place in the phonebook
+    for(int i=0;i<phoneBookSize;i++){
+        if(names[i]==" "&&phones[i]==" "){
+            names[i]=name;
+            phones[i]=phone;
+            entryAddedSuccessfully=true;
+            break;
+        }else{
+            continue;    
+        }
+    }
+if(entryAddedSuccessfully==true && isPhoneNumberValid==true){
+    return true;
+}
+}
 
 
 
