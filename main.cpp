ed659262adff46b7204e42ae9a4c15bcb26126f8
krypton1840 <youssefmohamed.1840@gmail.com>
2021-11-10 01:40:32 +0200

    Copying and commiting code from dev/Mostafa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <iomanip> // Needed for formatting output
 using namespace std;
 
 class MyPhoneBook
@@ -22,11 +21,7 @@ class MyPhoneBook
  bool updatePhoneAt(string, int);
 //  ~MyPhoneBook();
 };
-
-
 //Constructors
-
-//Parametarized constructor 
 MyPhoneBook::MyPhoneBook(int phoneBookSize){
     this->phoneBookSize=phoneBookSize;
     names=new string[phoneBookSize];
@@ -37,7 +32,6 @@ MyPhoneBook::MyPhoneBook(int phoneBookSize){
         }
 }
 
-//Copy constructor
 MyPhoneBook::MyPhoneBook(const MyPhoneBook &phoneBook){
     phoneBookSize=phoneBook.phoneBookSize;
     names=new string[phoneBookSize];
@@ -48,7 +42,7 @@ MyPhoneBook::MyPhoneBook(const MyPhoneBook &phoneBook){
     }
 }
 
-//Add Entry function
+//Add Entry
 
 bool MyPhoneBook::addEntry(string name ,string phone){
     bool entryAddedSuccessfully;
@@ -84,58 +78,92 @@ bool MyPhoneBook::addEntry(string name ,string phone){
             continue;    
         }
     }
-    if(entryAddedSuccessfully==true && isPhoneNumberValid==true){
-        return true;
-    }else{
+if(entryAddedSuccessfully==true && isPhoneNumberValid==true){
+    return true;
+}else{
+    return false;
+}
+}
+
+// displayEntryAtIndex
+bool MyPhoneBook::displayEntryAtIndex(int index){
+    if(index>(phoneBookSize-1) || index<0){
         return false;
     }
-}
-
-// Function to display all entries.
-void MyPhoneBook::displayAll()
-{
-    for (int i = 0; i < phoneBookSize; i++)
-    {
-        // If conditon to skip blank entries
-        if(names[i] == " " && phones[i] == " ")
-        {
-            continue;
-        }
-        else 
-        {
-            cout << setw(20) << left << names[i];
-            cout << setw(10) << phones[i];
-            cout << endl;
-        }
-        
+    else{
+        cout<<"Name : "<<names[index]<<endl;
+        cout<<"Phone number : "<<phones[index]<<endl;
+        return true;
     }
-    
 }
 
+// findBy
 
+// findByName
+int * MyPhoneBook::findByName(string name){
+    // entry.find(specific string)
+    int * findNameArray=new int[phoneBookSize];
+    for(int i=0;i<phoneBookSize;i++){
+        if(names[i].rfind(name,0)!=string::npos){
+            findNameArray[i]=1;
+        }else{
+            findNameArray[i]=0;
+        }
+    }
+    return findNameArray;
+
+}
+
+// findByPhone
+int * MyPhoneBook::findByPhone(string phone){
+    // entry.find(specific string)
+    int * findPhoneArray=new int[phoneBookSize];
+    for(int i=0;i<phoneBookSize;i++){
+        if(phones[i].rfind(phone,0)!=string::npos){
+            findPhoneArray[i]=1;
+        }else{
+            findPhoneArray[i]=0;
+        }
+    }
+    return findPhoneArray;
+
+}
 
 
 int main(){
     //Used for testing functionality
 
     MyPhoneBook pb1(2);
-    bool result1=pb1.addEntry("Mostafa Ibrahim","12345678901");//11 valid and empty space available == true
+    bool result1=pb1.addEntry("Mostafa Ibrahim","01045678901");//11 valid and empty space available == true
     cout<<"Entry 1: "<<result1<<endl;
     bool result2=pb1.addEntry("Mostafa","12345)78#01");//11 not valid and empty space available == false
     cout<<"Entry 2: "<<result2<<endl;
     bool result3=pb1.addEntry("Mostafa","123456708901");//not 11 but valid and empty space available == false
     cout<<"Entry 3: "<<result3<<endl;
-    bool result4=pb1.addEntry("Mostafa","12345778901");//11 valid and empty space available == true
+    bool result4=pb1.addEntry("M.Ibrahim Abdellatif","01134578900");//11 valid and empty space available == true
     cout<<"Entry 4: "<<result4<<endl;
-    bool result5=pb1.addEntry("Mostafa","223456708901");//11 valid and empty space NOT available == false
+    bool result5=pb1.addEntry("Mostafa","01010101010");//11 valid and empty space NOT available == false
     cout<<"Entry 5: "<<result5<<endl;
-
+    
     // Entry 1: 1
     // Entry 2: 0
     // Entry 3: 0
     // Entry 4: 1
     // Entry 5: 0
 
-    pb1.displayAll();
+    //testing displayEntryAtIndex
+    //If index is out of range will return false and won't display an entry
+    //If index is in range then it will return true and will display the entry of that index
+    bool res=pb1.displayEntryAtIndex(1);
+    cout<<"display Entry :"<<res<<endl;
+
+    //testing findBy
+    int *resArray=pb1.findByName("M");
+    cout<<resArray[0]<<endl;
+    cout<<resArray[1]<<endl;
+    cout<<"Phone :"<<endl;
+    int *resArray2=pb1.findByPhone("010");
+    cout<<resArray2[0]<<endl;
+    cout<<resArray2[1]<<endl;
     return 0;
 }
