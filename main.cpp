9ab799df6351621c9a09304a51f0227352b559ad
krypton1840 <youssefmohamed.1840@gmail.com>
2021-11-11 16:49:16 +0200

    Main function prototype using switch

--- main.cpp
+++ main.cpp
@@ -267,66 +267,17 @@ void MyPhoneBook::displayEntryAtIndices(int *indicesArray)
     
 }
 
-int main(){
-    //Used for testing functionality
-
-    MyPhoneBook pb1(2);
-    bool result1=pb1.addEntry("Mostafa Ibrahim","01045678901");//11 valid and empty space available == true
-    
-    bool result2=pb1.addEntry("Mostafa","12345)78#01");//11 not valid and empty space available == false
-    
-    bool result3=pb1.addEntry("Mostafa","123456708901");//not 11 but valid and empty space available == false
-    
-    bool result4=pb1.addEntry("Mostafa Ibrahim","01134578900");//11 valid and empty space available == true
-    
-    bool result5=pb1.addEntry("Mostafa","01010101010");//11 valid and empty space NOT available == false
-    /*
-    // test for displayEntryAtIndices
-    int *arr = new int [2];
-    for (int i = 0; i < 2; i++)
-    {
-        cin >> arr[i];
-    }
-    
-    pb1.displayEntryAtIndices(arr);
-    */
-    
-    // Entry 1: 1
-    // Entry 2: 0
-    // Entry 3: 0
-    // Entry 4: 1
-    // Entry 5: 0
-
-    //testing displayEntryAtIndex
-    //If index is out of range will return false and won't display an entry
-    //If index is in range then it will return true and will display the entry of that index
-    bool res=pb1.displayEntryAtIndex(1);
-    cout<<" "<<endl;
-
-    //testing findBy
-    int *resArray=pb1.findByName("M");
-    int *resArray2=pb1.findByPhone("010");
-
-    // to test updateNameAt
-    bool test = pb1.updateNameAt("Youssef",0);
-    pb1.displayEntryAtIndex(0);
-
-    //to test updatePhoneAt
-    bool test2 = pb1.updatePhoneAt("12345678900",0);
-    pb1.displayEntryAtIndex(0);
-
-    cout<<"----"<<endl;
-    pb1.displayAll();
-    
-    //--------------------------
-    cout<<""<<endl;
-    cout<<"Program start!"<<endl;
-    cout<<""<<endl;
+int main()
+{
     int sizeOfPhoneBook;
-    cout<<"Enter the size of your Phone Book: ";
-    cin>>sizeOfPhoneBook;
+    bool isProgramRunning = true;
+    cout << "Enter the size of your phone: ";
+    cin >> sizeOfPhoneBook;
+
     MyPhoneBook userPhoneBook(sizeOfPhoneBook);
-    for(int i=0;i<sizeOfPhoneBook;i++){
+
+    for(int i=0;i<sizeOfPhoneBook;i++)
+    {
         string name;
         string phone;
         cout<<"Enter name "<<to_string(i+1)<<": ";
@@ -335,12 +286,13 @@ int main(){
         cin>>phone;
         userPhoneBook.addEntry(name,phone);
     }
-    cout<<""<<endl;
 
-    bool programIsRunning=true;
-
-    while(programIsRunning){
+    do
+    {
         string userChoice;
+        string allowedPhoneNumberCharacters="12345678";
+        bool isUserChoiceValid;
+
         cout<<""<<endl;
         cout<<""<<endl;
         cout<<"Enter your choice: "<<endl;
@@ -354,25 +306,89 @@ int main(){
         cout<<"8- Exit"<<endl;
         cout<<"Choice: ";
         cin>>userChoice;
-        cout<<""<<endl;
-        if(userChoice=="1"){cout<<"Display all phone book"<<endl;}
-        else if(userChoice=="2"){cout<<"Search by name"<<endl;}
-        else if(userChoice=="3"){cout<<"Search by phone"<<endl;}
-        else if(userChoice=="4"){cout<<"find entry by index"<<endl;}
-        else if(userChoice=="5"){cout<<"update name by index"<<endl;}
-        else if(userChoice=="6"){cout<<"update phone by index"<<endl;}
-        else if(userChoice=="7"){cout<<"copy pb to another and display entries"<<endl;}
-        else if(userChoice=="8"){
-            programIsRunning=false;
-            cout<<"exit"<<endl;}
-        else{cout<<"Choice not available!"<<endl;}
+
+        // to check user's input.
+        if(userChoice.length()==1)
+        {
+            
+            if(allowedPhoneNumberCharacters.find(userChoice[0])!=string::npos)
+            {
+                isUserChoiceValid=true;
+            }
+            else
+            {
+                isUserChoiceValid=false;
+                break;
+            }
+            
+        }
+        else
+        {
+            isUserChoiceValid=false;
+        }
+        
+        // Converting input of user to int.
+        int finalChoice = stoi(userChoice);
+
+        if (isUserChoiceValid)  
+        {
+            switch (finalChoice)
+            {
+                case 1:
+                    cout << "Display all phone book" << endl;
+                    //userPhoneBook.displayAll();
+                    break;
+
+                case 2:
+                    cout<<"Search by name" << endl;
+                    //userPhoneBook.findByName();
+                    break;
+                
+                case 3:
+                    cout<<"Search by phone" << endl;
+                    //userPhoneBook.findByPhone();
+                    break;
+                
+                case 4:
+                    cout<<"display entry by index" << endl;
+                    //userPhoneBook.displayEntryAtIndex();
+                    break;
+                
+                case 5:
+                    cout << "Update name by index" << endl;
+                    //userPhoneBook.updateNameAt();
+                    break;
+
+                case 6:
+                    cout << "Update phone by index" << endl;
+                    //userPhoneBook.updatePhoneAt();
+                    break;
+                
+                case 7:
+                    cout << "copy pb to another and display entries" << endl;
+                    /* 1- create a new phone book
+                       2- copy the contents of old phonebook using constructor.
+                       3- display all()
+                    */
+                    break;
+
+                case 8:// Not mentioned in the assignment file.
+                    cout << "Display entries at indices" << endl;
+                    //userPhoneBook.displayEntryAtIndices();
+                    break;
+
+                case 9:
+                    isProgramRunning = false;
+                    cout << "exit" << endl;
+                    break;
+                
+            }
+        }
+        
 
 
-    }
+    } while (isProgramRunning);
     
     
     return 0;
-
-
-
 }
